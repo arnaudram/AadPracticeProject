66d07e7cc1cf91347fe459902896fc55caf2659d
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-09-11 10:25:17 +0300

    cleaning-and-submitting-my-project

--- app/src/main/java/com/example/aadproject/SubmissionActivity.kt
+++ app/src/main/java/com/example/aadproject/SubmissionActivity.kt
@@ -1,23 +1,24 @@
 package com.example.aadproject
 
-import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Intent
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+
 import android.widget.ProgressBar
-import android.widget.TextView
+
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.aadproject.data.EntryGoogleForm
 import com.example.aadproject.databinding.ActivitySubmissionBinding
 import com.example.aadproject.fragments.ProceedSubmissionDialog
+import com.example.aadproject.services.GoogleFormApi
 import com.example.aadproject.viewmodels.ObserveDialog
-import com.example.aadproject.viewmodels.SubmissionViewModel
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SubmissionActivity : AppCompatActivity() {
     lateinit var entryGoogleForm: EntryGoogleForm
@@ -29,56 +30,25 @@ class SubmissionActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_submission)
 
+
         setSupportActionBar(binding.toolbar)
         supportActionBar?.apply {
             setDisplayHomeAsUpEnabled(true)
             title = ""
             setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_white_24)
         }
-        // viewModel
-        val submissionViewModel by lazy { ViewModelProvider(this)[SubmissionViewModel::class.java] }
 
-        binding.submissionViewModel = submissionViewModel
-        binding.lifecycleOwner = this
-        //observing dialog viewModel
         observeDialog = ViewModelProvider(this)[ObserveDialog::class.java]
-        submissionViewModel.onSubmit.observe(this, Observer {
-            if (it) {
-                if (proceed()) {
-                    if (fieldChecked()) {
-                        showProgressBar()
-                        submissionViewModel.apply {
-                            sendProject(entryGoogleForm)
-                            hideProgressBar()
-                            showUpResult(this)
 
-                        }
-
-                    }
-
-                }
-
-                submissionViewModel.doneSubmitting()
-
-            }
-        })
 
 
     }
 
-    private fun showUpResult(submissionViewModel: SubmissionViewModel) {
-        submissionViewModel.response.observe(this, Observer {
-            if (it) {
-                showSubmissionSuccessfulDialog()
-            } else {
-                showSubmissionFailledDialog()
-            }
-        })
-    }
 
-    private fun showSubmissionFailledDialog() {
+
+    private fun showSubmissionFailedDialog() {
         Dialog(this).apply {
-            setContentView(R.layout.submission_failled)
+            setContentView(R.layout.submission_failed)
 
         }.show()
     }
@@ -100,47 +70,86 @@ class SubmissionActivity : AppCompatActivity() {
 
     }
 
-    private fun proceed(): Boolean {
-        var areYouSure = false
-        ProceedSubmissionDialog().show(supportFragmentManager, "dialog_Proceed")
-        observeDialog.continueSubmissiion.observe(this, Observer {
-            areYouSure = it
-        })
 
 
-        return areYouSure
-    }
 
-    private fun fieldChecked(): Boolean {
-        var requirementMet = false
-        var firstName = binding.editFirstName.toString()
-        var lastName = binding.editLastName.toString()
-        var emailAddress = binding.editEmailAdress.toString()
-        var linkToProject = binding.editGithubLink.toString()
-        if (firstName.isNotEmpty()) {
-            requirementMet = true
-        } else {
-            binding.editFirstName.error = " required* "
-        }
-        if (lastName.isNotEmpty()) {
-            requirementMet = true
-        } else {
-            binding.editLastName.error = " required* "
-        }
-        if (emailAddress.isNotEmpty()) {
-            requirementMet = true
-        } else {
-            binding.editEmailAdress.error = " required* "
-        }
-        if (linkToProject.isNotEmpty()) {
-            requirementMet = true
-        } else {
-            binding.editGithubLink.error = " required* "
-        }
+    fun sendProject(view: View) {
+        var requirementMet = true
 
+        var firstName = binding.editFirstName.text.toString()
+        var lastName = binding.editLastName.text.toString()
+        var emailAddress = binding.editEmailAdress.text.toString()
+        var linkToProject = binding.editGithubLink.text.toString()
+        requirementMet =
+            checkField(firstName, requirementMet, lastName, emailAddress, linkToProject)
         if (requirementMet) {
             entryGoogleForm = EntryGoogleForm(firstName, lastName, emailAddress, linkToProject)
+
+            ProceedSubmissionDialog().show(supportFragmentManager, "dialog_Proceed")
+
+            observeDialog.continueSubmissiion.observe(this, Observer {
+                if (it) {
+                        send(entryGoogleForm)
+
+                    observeDialog.onNegative()
+                }
+
+            })
+
+
         }
-        return requirementMet
+    }
+
+    private fun checkField(
+        firstName: String,
+        requirementMet: Boolean,
+        lastName: String,
+        emailAddress: String,
+        linkToProject: String
+    ): Boolean {
+        var requirementMet1 = requirementMet
+        if (firstName.isEmpty()) {
+            requirementMet1 = false
+            binding.textInputLayout.error = " required* "
+
+
+        }
+
+        if (lastName.isEmpty()) {
+            requirementMet1 = false
+            binding.inputLayoutLastName.error = " required* "
+        }
+
+        if (emailAddress.isEmpty()) {
+            requirementMet1 = false
+            binding.inputLayoutAdressEmail.error = " required* "
+        }
+        if (linkToProject.isEmpty()) {
+            requirementMet1 = false
+            binding.inputLayoutGithub.error = " required* "
+        }
+        return requirementMet1
+    }
+
+
+    private fun send(entryGoogleForm: EntryGoogleForm){
+        showProgressBar()
+          GoogleFormApi.retrofitService.sendCurrentProject(entryGoogleForm.fisrtName,entryGoogleForm.lastName
+            ,entryGoogleForm.emailAddress,entryGoogleForm.linkToProject)
+        .enqueue(object: Callback<EntryGoogleForm> {
+            override fun onFailure(call: Call<EntryGoogleForm>, t: Throwable) {
+                hideProgressBar()
+                showSubmissionFailedDialog()
+
+            }
+
+            override fun onResponse(
+                call: Call<EntryGoogleForm>,
+                response: Response<EntryGoogleForm>
+            ) {
+                hideProgressBar()
+                showSubmissionSuccessfulDialog()
+            }
+        })
     }
 }
